8528c61fe0bde3b83844ba8c2aa65802304f0979
George Haworth <ghaworth1@gmail.com>
2023-09-02 12:31:10 -0500

    idk, testing?

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -16,7 +16,34 @@ module.exports = () => {
     },
 
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: "J.A.T.E."
+      }),
 
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name:'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: "Just Another Text Editor",
+        background_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons:
+            [
+              {
+                src: path.resolve('src/images/logo.png'),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join('assets', 'icons'),
+              },
+            ]
+      })
     /*
       TODO:
 
@@ -71,18 +98,22 @@ module.exports = () => {
 
     module: {
       rules: [
-
-        /*
-          TODO: In this array, we need to configure the rules 
-          for when Webpack should "bundle-ize" our files. Each rule 
-          is an object. We will be setting up two rules. 
-          
-          You will find both rules in the solution for the mini 
-          project. 
-
-          When you copy and paste them below, you'll be done here.
-        */
-
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
